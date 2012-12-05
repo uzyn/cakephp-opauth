6e9160555edd60a12763783f83c95d624c5a1752
U-Zyn Chua <chua@uzyn.com>
2012-12-05 08:19:51 +0800

    Updated plugin to work with AuthComponent by allowing access to all Opauth methods. Thanks @deewilcox #12

--- Controller/OpauthController.php
+++ Controller/OpauthController.php
@@ -1,3 +1,11 @@
 <?php
 class OpauthController extends OpauthAppController {
+
+	public function beforeFilter() {
+		// Allow access to Opauth methods for users of AuthComponent
+		if (is_object($this->Auth) && method_exists($this->Auth, 'allow')) {
+			$this->Auth->allow('*');
+		}
+
+	}
 }
