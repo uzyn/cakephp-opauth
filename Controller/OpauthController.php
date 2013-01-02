b1e2b976eb9ef2befac44b0e0e2eb1e831cb3d57
U-Zyn Chua <chua@uzyn.com>
2013-01-03 00:09:27 +0800

    Fixes a bug on AuthComponent syntax difference between CakePHP < 2.1 and >= 2.1.

--- Controller/OpauthController.php
+++ Controller/OpauthController.php
@@ -4,8 +4,12 @@ class OpauthController extends OpauthAppController {
 	public function beforeFilter() {
 		// Allow access to Opauth methods for users of AuthComponent
 		if (is_object($this->Auth) && method_exists($this->Auth, 'allow')) {
-			$this->Auth->allow('*');
+			if (version_compare(Configure::version(), '2.1.0', '>=')) {
+				$this->Auth->allow();
+			} else {
+				// CakePHP 2.0
+				$this->Auth->allow('*');
+			}
 		}
-
 	}
 }
