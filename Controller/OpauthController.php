a7e00591a3437040a452cd9f6ab54e452e3d6623
U-Zyn Chua <chua@uzyn.com>
2013-01-03 00:48:07 +0800

    Removed version_compare as CakePHP 2.0 AuthComponent works just fine with allow() [Thanks @ceeram]
    Ref.:
    https://github.com/cakephp/cakephp/blob/2.0.0/lib/Cake/Controller/Compon
    ent/AuthComponent.php#L431

--- Controller/OpauthController.php
+++ Controller/OpauthController.php
@@ -4,12 +4,7 @@ class OpauthController extends OpauthAppController {
 	public function beforeFilter() {
 		// Allow access to Opauth methods for users of AuthComponent
 		if (is_object($this->Auth) && method_exists($this->Auth, 'allow')) {
-			if (version_compare(Configure::version(), '2.1.0', '>=')) {
-				$this->Auth->allow();
-			} else {
-				// CakePHP 2.0
-				$this->Auth->allow('*');
-			}
+			$this->Auth->allow();
 		}
 	}
 }
