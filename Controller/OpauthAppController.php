0b6ac0c8e44fb6d877f440d350121c1f69acd7af
Jeroenvdvalk <jeroen-gijs@hotmail.com>
2018-02-27 10:45:30 +0100

    Fixed for breaking change in opauth/opauth 0.4.5.

--- Controller/OpauthAppController.php
+++ Controller/OpauthAppController.php
@@ -54,10 +54,10 @@ class OpauthAppController extends AppController {
 				}
 				break;
 			case 'post':
-				$response = unserialize(base64_decode( $_POST['opauth'] ));
+				$response = json_decode(base64_decode( $_POST['opauth'], true));
 				break;
 			case 'get':
-				$response = unserialize(base64_decode( $_GET['opauth'] ));
+				$response = json_decode(base64_decode( $_GET['opauth'], true));
 				break;
 			default:
 				echo '<strong style="color: red;">Error: </strong>Unsupported callback_transport.'."<br>\n";
